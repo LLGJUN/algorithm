f5e4b302da5b75d8025b4da6495bb5f63cd7f8bc
LLGJUN <1360622136@qq.com>
2017-09-13 05:25:16 +0000

    lianbiao file

--- lianbiao.c
+++ lianbiao.c
@@ -1,3 +1,6 @@
+//*****************************************//
+/*delete the element in a array */
+//*****************************************//
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
